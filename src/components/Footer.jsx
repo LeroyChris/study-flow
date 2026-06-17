e135491dab2c206c0491690c3be37e79db3b10f1
Faza Qinthoro Putra <cyberpunkcrime@gmail.com>
2026-06-17 07:07:32 +0700

    feat: improve landing page, task board, and flashcard interactions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-brand-warm text-black py-6 text-center mt-20">
+    <footer className="text-black py-6 text-left pl-20 mt-20">
       <p>&copy; 2024 StudyFlow. All rights reserved.</p>
     </footer>
   )
---
+++ src/components/TiltCard.jsx
@@ -0,0 +1,38 @@
+// components/TiltCard.jsx
+
+import { useTilt } from '../hooks/useTilt'
+
+export default function TiltCard({
+  children,
+  className = '',
+  onClick,
+}) {
+  const {
+    rotation,
+    handleMouseMove,
+    reset,
+  } = useTilt(40)
+
+  return (
+    <div
+      onMouseMove={handleMouseMove}
+      onMouseLeave={reset}
+      onClick={onClick}
+      style={{
+        transform: `
+          perspective(1000px)
+          rotateY(${rotation.x}deg)
+          rotateX(${-rotation.y}deg)
+        `,
+      }}
+      className={`
+        transition-transform
+        duration-150
+        will-change-transform
+        ${className}
+      `}
+    >
+      {children}
+    </div>
+  )
+}
